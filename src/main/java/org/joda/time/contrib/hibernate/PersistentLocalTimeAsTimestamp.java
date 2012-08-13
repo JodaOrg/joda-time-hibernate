35f5e4ea81530ec0ce343b1aff9a1ad2925b261a
Stephen Colebourne <scolebourne@joda.org>
2012-08-13 12:16:41 +0100

    Tidy patch

--- src/main/java/org/joda/time/contrib/hibernate/PersistentLocalTimeAsTimestamp.java
+++ src/main/java/org/joda/time/contrib/hibernate/PersistentLocalTimeAsTimestamp.java
@@ -19,97 +19,91 @@ import org.joda.time.LocalTime;
  * 
  * @author Daniel Jurado (jurado@gmail.com)
  */
-public class PersistentLocalTimeAsTimestamp implements EnhancedUserType,
-		Serializable {
-	public static final PersistentLocalTimeAsTimestamp INSTANCE = new PersistentLocalTimeAsTimestamp();
+public class PersistentLocalTimeAsTimestamp implements EnhancedUserType, Serializable {
 
-	private static final int[] SQL_TYPES = new int[] { Types.TIMESTAMP, };
+  public static final PersistentLocalTimeAsTimestamp INSTANCE = new PersistentLocalTimeAsTimestamp();
 
-	public Object assemble(Serializable cached, Object value)
-			throws HibernateException {
-		return cached;
-	}
+    private static final int[] SQL_TYPES = new int[] { Types.TIMESTAMP, };
 
-	public Object deepCopy(Object value) throws HibernateException {
-		return value;
-	}
+    public Object assemble(Serializable cached, Object value) throws HibernateException {
+        return cached;
+    }
 
-	public Serializable disassemble(Object value) throws HibernateException {
-		return (Serializable) value;
-	}
+    public Object deepCopy(Object value) throws HibernateException {
+        return value;
+    }
 
-	public boolean equals(Object x, Object y) throws HibernateException {
-		if (x == y) {
-			return true;
-		}
-		if (x == null || y == null) {
-			return false;
-		}
-		LocalTime dtx = (LocalTime) x;
-		LocalTime dty = (LocalTime) y;
-		return dtx.equals(dty);
-	}
+    public Serializable disassemble(Object value) throws HibernateException {
+        return (Serializable) value;
+    }
 
-	public Object fromXMLString(String string) {
-		return new LocalTime(string);
-	}
+    public boolean equals(Object x, Object y) throws HibernateException {
+        if (x == y) {
+            return true;
+        }
+        if (x == null || y == null) {
+            return false;
+        }
+        LocalTime dtx = (LocalTime) x;
+        LocalTime dty = (LocalTime) y;
+        return dtx.equals(dty);
+    }
 
-	public int hashCode(Object object) throws HibernateException {
-		return object.hashCode();
-	}
+    public Object fromXMLString(String string) {
+        return new LocalTime(string);
+    }
 
-	public boolean isMutable() {
-		return false;
-	}
+    public int hashCode(Object object) throws HibernateException {
+        return object.hashCode();
+    }
 
-	public Object nullSafeGet(ResultSet resultSet, String string)
-			throws SQLException {
-		Object timestamp = StandardBasicTypes.TIMESTAMP.nullSafeGet(resultSet,
-				string);
-		if (timestamp == null) {
-			return null;
-		}
+    public boolean isMutable() {
+        return false;
+    }
 
-		return new LocalTime(timestamp, DateTimeZone.UTC);
-	}
+    public Object nullSafeGet(ResultSet resultSet, String string) throws SQLException {
+        Object timestamp = StandardBasicTypes.TIMESTAMP.nullSafeGet(resultSet, string);
+        if (timestamp == null) {
+            return null;
+        }
 
-	public Object nullSafeGet(ResultSet resultSet, String[] strings,
-			Object object) throws HibernateException, SQLException {
-		return nullSafeGet(resultSet, strings[0]);
+        return new LocalTime(timestamp, DateTimeZone.UTC);
+    }
 
-	}
+    public Object nullSafeGet(ResultSet resultSet, String[] strings, Object object) throws HibernateException, SQLException {
+        return nullSafeGet(resultSet, strings[0]);
 
-	public void nullSafeSet(PreparedStatement preparedStatement, Object value,
-			int index) throws HibernateException, SQLException {
-		if (value == null) {
-			StandardBasicTypes.TIMESTAMP.nullSafeSet(preparedStatement, null,
-					index);
-		} else {
-			LocalTime lt = ((LocalTime) value);
-			Timestamp timestamp = new Timestamp(lt.getMillisOfDay());
-			StandardBasicTypes.TIMESTAMP.nullSafeSet(preparedStatement,
-					timestamp, index);
-		}
-	}
+    }
 
-	public String objectToSQLString(Object object) {
-		throw new UnsupportedOperationException();
-	}
+    public void nullSafeSet(PreparedStatement preparedStatement, Object value, int index) throws HibernateException, SQLException {
+        if (value == null) {
+            StandardBasicTypes.TIMESTAMP.nullSafeSet(preparedStatement, null,
+                    index);
+        } else {
+            LocalTime lt = ((LocalTime) value);
+            Timestamp timestamp = new Timestamp(lt.getMillisOfDay());
+            StandardBasicTypes.TIMESTAMP.nullSafeSet(preparedStatement,
+                    timestamp, index);
+        }
+    }
 
-	public Object replace(Object original, Object target, Object owner)
-			throws HibernateException {
-		return original;
-	}
+    public String objectToSQLString(Object object) {
+        throw new UnsupportedOperationException();
+    }
 
-	public Class<?> returnedClass() {
-		return LocalTime.class;
-	}
+    public Object replace(Object original, Object target, Object owner) throws HibernateException {
+        return original;
+    }
 
-	public int[] sqlTypes() {
-		return SQL_TYPES;
-	}
+    public Class<?> returnedClass() {
+        return LocalTime.class;
+    }
 
-	public String toXMLString(Object object) {
-		return object.toString();
-	}
+    public int[] sqlTypes() {
+        return SQL_TYPES;
+    }
+
+    public String toXMLString(Object object) {
+        return object.toString();
+    }
 }
